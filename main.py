a32b68c10f39fa887660b7ed57b8826ca6c733b3
Andrea <zorzi.andrea@outlook.com>
2024-07-05 12:17:44 +0200

    build(TextToImage): added 3D BitMap to handle RGB

--- main.py
+++ main.py
@@ -25,16 +25,16 @@ class TextToImage:
         """
         width, height, baseline = 0, 0, 0
         previous = 0
-        for letter in text:
-            self.face.load_char(letter)
+        for i, c in enumerate(text):
+            self.face.load_char(c)
             bitmap = self.slot.bitmap
             height = max(
                 height, bitmap.rows + max(0, -(self.slot.bitmap_top - bitmap.rows))
             )
             baseline = max(baseline, max(0, -(self.slot.bitmap_top - bitmap.rows)))
-            kerning = self.face.get_kerning(previous, letter)
-            width += self.slot.advance.x >> 6 + kerning.x >> 6
-            previous = letter
+            kerning = self.face.get_kerning(previous, c)
+            width += (self.slot.advance.x >> 6) + (kerning.x >> 6)
+            previous = c
         return width, height, baseline
 
     def render_text(self, text: str) -> np.ndarray:
@@ -45,23 +45,31 @@ class TextToImage:
         :return: Numpy array representing the rendered image.
         """
         width, height, baseline = self.calculate_image_size(text)
-        image = np.zeros((height, width), dtype=np.ubyte)
+
+        image = np.zeros((height, width, 3), dtype=np.ubyte)
 
         x, y = 0, 0
         previous = 0
-        for letter in text:
-            self.face.load_char(letter)
+        for c in text:
+            self.face.load_char(c)
+            bitmap = self.slot.bitmap
             top = self.slot.bitmap_top
             left = self.slot.bitmap_left
-            w, h = self.slot.bitmap.width, self.slot.bitmap.rows
+            w, h = bitmap.width, bitmap.rows
             y = height - baseline - top
-            kerning = self.face.get_kerning(previous, letter)
+            kerning = self.face.get_kerning(previous, c)
             x += kerning.x >> 6
-            image[y : y + h, x + left : x + left + w] += np.array(
-                self.slot.bitmap.buffer, dtype="ubyte"
-            ).reshape(h, w)
+
+            bitmap_2d = np.array(bitmap.buffer, dtype="ubyte").reshape(h, w)
+
+            bitmap_3d = np.zeros((h, w, 3), dtype="ubyte")
+            bitmap_3d[:, :, 0] = bitmap_2d  # Canale rosso
+            bitmap_3d[:, :, 1] = bitmap_2d  # Canale verde
+            bitmap_3d[:, :, 2] = bitmap_2d  # Canale blu
+
+            image[y : y + h, x + left : x + left + w] += bitmap_3d
             x += self.slot.advance.x >> 6
-            previous = letter
+            previous = c
 
         return image
 
@@ -71,7 +79,7 @@ def main():
     Main function to execute the text rendering and save the output image.
     """
     text_to_image = TextToImage("font.ttf", 48)
-    text = "Elena stai bene?"
+    text = "Elena stai beneeasdfasdfasdfasdfs?"
     image = text_to_image.render_text(text)
     cv.imwrite("result.jpg", image)
 
